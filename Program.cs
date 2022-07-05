87f49f0c8c77924bd03d8dc718dbafa79bcb1186
Yogesh <yogeshbietjhs@gmail.com>
2022-07-05 10:11:34 +0530

    inicial commit

--- Program.cs
+++ Program.cs
@@ -2,13 +2,11 @@
 {
     public class Program
     {
-        /// <summary>
-        /// Greeting message to welcome in this program.
-        /// </summary>
+        
         public static void Main(string[] args)
         {
 
-            Console.WriteLine("Welcome to Employee Wage Computation Program\n");
+            Console.WriteLine("Welcome to Employee Wage Computation Program");
             EmpWage company1 = new EmpWage(20, 150, 40, "Apple");
             EmpWage company2 = new EmpWage(20, 140, 38, "Microsoft");
             EmpWage company3 = new EmpWage(18, 145, 42, "Google");
